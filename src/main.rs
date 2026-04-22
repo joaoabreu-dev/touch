2017b5306941f6baae78c1f54bb8e751551ab235
GDPM05 <j.p.f.a009@gmail.com>
2026-04-22 14:11:48 +0100

    feat: add CLI flag support and HTML template capability
    - Implement manual argument parsing in Config::build
    - Add validation for flags (currently supports -H for HTML)
    - Improve filename identification by checking prefixes
    - Update Config struct to store detected flags

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,9 @@ fn main() {
         eprintln!("{}", err);
         process::exit(1);
     });
-    
+   
+    println!("config: {:?}", config);
+
     create_file(config.file_name.as_str()).unwrap_or_else(|err| {
         eprintln!("{}", err);
         process::exit(1);
@@ -20,25 +22,62 @@ fn main() {
 
 }
 
+#[derive(Debug)]
+enum Flags {
+    H, 
+    I,
+    P,
+    F,
+    K,
+}
+
+#[derive(Debug)]
 struct Config {
-    file_name: String
+    file_name: String,
+    flags: Option<Vec<Flags>>
 }
 
 impl Config {
     fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
         args.next(); 
 
-        let file_name = match args.next() {
-            Some(arg) => arg,
-            None => return Err("Por favor indique um nome para o ficheiro!"),
-        };
+        let mut flags: Vec<Flags> = Vec::new();
+        let mut file_name: Option<String> = None;
+
+        for arg in args {
+            if arg.starts_with("-") {
+                println!("{}", &arg[1..]);
+                let arg = match &arg[1..] {
+                    "H" => Some(Flags::H),
+                    "F" => Some(Flags::F),
+                    "I" => Some(Flags::I),
+                    "K" => Some(Flags::K),
+                    _ => None
+                };
+                flags.push(arg.ok_or("Flag inválida!")?);    
+            } else {
+                file_name = Some(arg);
+            }
+        }
+       
+        if file_name.is_none() {
+            return Err("Indique um nome para o ficheiro");
+        }
+    
+        let file_name: String = file_name.unwrap();
 
         let regex = Regex::new("[<>:\"/\\\\|?*]").unwrap();
 
         if regex.is_match(&file_name) {
             return Err("Caracteres inválidos no nome do ficheiro!");
         }
+        
+        if flags.len() > 0 {
+            Ok(Config { file_name, flags: Some(flags) })
+        } else {
+            Ok(Config { file_name, flags: None })
+        }
 
-        Ok(Config { file_name })
     }
+
 }
